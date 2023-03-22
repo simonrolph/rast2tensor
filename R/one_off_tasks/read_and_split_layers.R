78818a357f41ddb1d32b8931bd67dd2ae84a0081
Simon Rolph <simon.p.rolph@gmail.com>
2023-03-22 09:57:13 +0000

    folder for hpc logs

--- R/one_off_tasks/read_and_split_layers.R
+++ R/one_off_tasks/read_and_split_layers.R
@@ -1,3 +1,5 @@
+# these are 'run once' bits of code that are used to load in rasters and export them as 1 file by band GeoTIFFs. These tifs are saved in data/raster
+
 library(terra)
 
 
@@ -13,19 +15,27 @@ writeRaster(env_data,paste0("data/raster/env_data_",names(env_data),".tif"))
 
 
 
-
-
-
 # other files
-lc_data <- rast("C:\\Users\\simrol\\Documents\\R\\DECIDE_WP1\\data\\derived_data\\environmental\\lcm2015gb100perc.tif")
+# lc_data <- rast("C:\\Users\\simrol\\Documents\\R\\DECIDE_WP1\\data\\derived_data\\environmental\\lcm2015gb100perc.tif")
+# 
+# names(lc_data)
+# writeRaster(lc_data,paste0("data/raster/land_cover_",names(lc_data),".tif"))
+# 
+# 
+# el_data <- rast("C:\\Users\\simrol\\Documents\\R\\DECIDE_WP1\\data\\derived_data\\environmental\\elevation_UK.grd")
+# el_data
+# 
+# el_data <- el_data %>% extend(lc_data)
+# writeRaster(env_data,"data/raster/elevation.tif")
 
-names(lc_data)
-writeRaster(lc_data,paste0("data/raster/land_cover_",names(lc_data),".tif"))
+
+#sentinel 2 data downloaded from GEE
+s2_data <-  rast("data/raw/sentinel2_bands.tif")
+crop_to <- rast("data/raster/env_data_bog.tif")
+s2_data_cropped <-terra::crop(x = s2_data,y = crop_to)
+writeRaster(s2_data_cropped,paste0("data/raster/s2_data_",names(s2_data_cropped),".tif"))
 
 
-el_data <- rast("C:\\Users\\simrol\\Documents\\R\\DECIDE_WP1\\data\\derived_data\\environmental\\elevation_UK.grd")
-el_data
 
-el_data <- el_data %>% extend(lc_data)
-writeRaster(env_data,"data/raster/elevation.tif")
+
 
---
+++ hpc_logs/README.md
@@ -0,0 +1,1 @@
+This is where the HPC error and output logs will go
